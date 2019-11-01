36226d581b4fff68a208e90265733e2d8238a7ca
Hugo Antunes <hugo.santunes@gmail.com>
2019-11-01 13:18:02 +0100

    update install.bash

--- install.sh
+++ install.sh
@@ -8,10 +8,6 @@ echo ".vim/ folder"
 rm -rf ~/.vim
 ln -s `pwd`/vim ~/.vim
 
-echo ".gitconfig"
-rm -f ~/.gitconfig
-ln -s `pwd`/gitconfig ~/.gitconfig
-
 echo ".profile"
 rm -f ~/.profile
 ln -s `pwd`/profile ~/.profile
